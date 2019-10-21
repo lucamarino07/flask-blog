0ea3b51e59436a8e80087c1fb81ee61fa076f242
Luca Marino <marino.luca07@gmail.com>
2019-10-21 22:03:09 +0200

    Initial commit

---
+++ blog/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app=app, db=db)
+login_manager = LoginManager(app)
+
+with app.app_context():
+    if db.engine.url.drivername == 'sqlite':
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
+
+from blog import models, routes
